302a9f14dd9c86d67351c682ad5e2b994a8444b4
milap-magar <milapmagar12@gmail.com>
2024-02-12 17:20:31 +0545

    error managed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,37 +5,34 @@ import "./components.css";
 import Button from "./Button";
 
 const Navbar = () => {
-
   return (
     <>
       <div className="navbar-section relative w-full h-28">
         <figure className="logo absolute top-4 left-6">
-          <a href="/">
-            <img
-              src={image}
-              alt="Logo image"
-              className="h-16 w-16"
-            />
+          <a href="#">
+            <img src={image} alt="Logo image" className="h-16 w-16" />
           </a>
         </figure>
         <div className="navbar-items absolute top-7 right-10">
           <ul className="list-items flex justify-center items-center gap-8">
             <li>
-              <a href="/">Home</a>
+              <a href="#">Home</a>
             </li>
             <li>
-              <a href="/works">Works</a>
+              <a href="#">Works</a>
             </li>
             <li>
-              <a href="/about">About</a>
+              <a href="#">About</a>
             </li>
             <li>
-              <a href="#"><Button name="Contact Me" /></a>
+              <a href="#">
+                <Button name="Contact Me" />
+              </a>
             </li>
           </ul>
-          <button className="btn-nav">
-            {/* {Sidebar ? <FaTimes /> : <FaBars />} */}
-          </button>
+          {/* <button className="btn-nav"> */}
+          {/* {Sidebar ? <FaTimes /> : <FaBars />} */}
+          {/* </button> */}
         </div>
       </div>
     </>
