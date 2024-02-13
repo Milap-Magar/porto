175446d6d7bbdcf487505a5a90b510c1e3bfa4f4
milap-magar <milapmagar12@gmail.com>
2024-02-13 08:00:49 +0545

    Logo managed

--- src/components/jsx/Footer.jsx
+++ src/components/jsx/Footer.jsx
@@ -5,18 +5,35 @@ import image from "../../assets/Logo.svg";
 import "../css/components.css";
 
 const Footer = () => {
+  // const emailLink = "mailto:milapmagar12@gmail.com";
+
   return (
     <>
       <div className="footer-section w-full h-[120px] relative">
         <figure className="logo-section">
-          <img src={ image } alt="Logo/Image" className="h-14 w-14 absolute top-4 left-5" />
+          <img
+            src={image}
+            alt="Logo/Image"
+            className="h-14 w-14 absolute top-4 left-5"
+          />
           <h6 id="footer-heading">©2023 Milap, All rights reserved.</h6>
         </figure>
         <div className="social-handles flex items-center absolute top-7 right-2 gap-3 cursor-pointer">
-          <FaLinkedin className="h-6 w-6" />
-          <FaFacebook className="h-6 w-6" />
-          <BiLogoGmail className="h-6 w-6" />
-          <FaGithubSquare className="h-6 w-6" />
+          <a
+            href="https://www.linkedin.com/in/milap-magar-21427a229/"
+            target="_blank"
+          >
+            <FaLinkedin className="h-6 w-6" />
+          </a>
+          <a href="https://www.facebook.com/Milap.Magar2022" target="_blank">
+            <FaFacebook className="h-6 w-6" />
+          </a>
+          <a href="https://mail.google.com/mail/u/0/#inbox" target="_blank">
+            <BiLogoGmail className="h-6 w-6" />
+          </a>
+          <a href="https://github.com/Milap-Magar" target="_blank">
+            <FaGithubSquare className="h-6 w-6" />
+          </a>
         </div>
       </div>
     </>
