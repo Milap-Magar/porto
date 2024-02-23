25c65f373452e0f21a4d32a6289994b21e1afbe6
milap-magar <milapmagar12@gmail.com>
2024-02-23 20:36:47 +0545

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Homepage, Works, About, Contact } from "./pages/index";
 import { Footer } from "./components/jsx";
-
+ 
 const App = () => {
   return (
     <>
