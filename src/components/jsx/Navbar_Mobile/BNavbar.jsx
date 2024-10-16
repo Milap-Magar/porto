950d7df02ec20993651d6d3cae69791fdb8c953e
milap-magar <milapmagar12@gmail.com>
2024-10-16 21:51:25 +0545

    Feat: Mobile navbar section added

--- src/components/jsx/Navbar_Mobile/BNavbar.jsx
+++ src/components/jsx/Navbar_Mobile/BNavbar.jsx
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import HomeIcon from "../../../assets/Home.svg";
-import ContactIcon from "../../../assets/Contact.svg";
-import ProjectIcon from "../../../assets/Project.svg";
+import ContactIcon from "../../../assets/contact1.svg";
+import ProjectIcon from "../../../assets/project1.svg";
 import oval from "../../../assets/navoval.svg";
 
 const BNavbar = () => {
   const [activePage, setActivePage] = useState("home");
-
-  const ovalPosition = {
-    home: "translate-x-[-120%]", // Adjust these values as needed for precise placement
-    project: "translate-x-[0%]",
-    contact: "translate-x-[120%]",
-  };
 
   const buttonVariants = {
     initial: { scale: 1 },
@@ -26,8 +20,7 @@ const BNavbar = () => {
   };
 
   return (
-    <div>
-      {/* Page Content */}
+    <div className="">
       <motion.div
         initial="out"
         animate="in"
@@ -41,27 +34,68 @@ const BNavbar = () => {
         {activePage === "contact" && <div>Contact Page</div>}
       </motion.div>
 
-      {/* Navigation Bar */}
-      <nav className="fixed bottom-0 w-full bg-black h-[72px] flex items-center justify-around z-50">
-        <div className="relative flex w-full h-full justify-around items-center">
+      <nav className="fixed bottom-0 w-full bg-black h-[72px] flex items-center justify-around z-50 ">
+        <div className="relative flex w-full h-full justify-center gap-16 items-center">
+          <motion.div
+            className={`absolute bottom-0 w-[102px] h-[105px]`}
+            initial={false}
+            animate={{
+              x:
+                activePage === "home"
+                  ? "-90%"
+                  : activePage === "project"
+                  ? "0%"
+                  : "90%",
+            }}
+            transition={{ type: "spring", stiffness: 120, damping: 15 }}
+            style={{ zIndex: 1 }}
+          >
+            <motion.img
+              src={oval}
+              alt="Oval"
+              className="w-[102px] h-[96px] absolute"
+              style={{
+                top: "-16px",
+              }}
+            />
+          </motion.div>
+
           {/* Home Icon */}
           <motion.div
-            className="h-8 w-8 relative"
+            className={`w-8 relative ${
+              activePage === "home" ? "z-10" : "animate z-0"
+            }`}
             onClick={() => setActivePage("home")}
+            initial={{ height: "20px" }}
+            animate={{ height: activePage === "home" ? "120%" : "45%" }}
+            transition={{
+              type: "spring",
+              stiffness: 120,
+              damping: 15,
+              duration: 0.5,
+            }}
             variants={buttonVariants}
-            initial="initial"
             whileHover="hover"
             whileTap="tap"
           >
-            <img src={HomeIcon} alt="Home" className="h-9 w-9" />
+            <img src={HomeIcon} alt="Home" className="h-8 w-14" />
           </motion.div>
 
           {/* Project Icon */}
           <motion.div
-            className="h-8 w-8 relative"
+            className={`w-[1.65rem] relative ${
+              activePage === "project" ? "z-10 " : "animate z-0"
+            }`}
             onClick={() => setActivePage("project")}
+            initial={{ height: "20px" }}
+            animate={{ height: activePage === "project" ? "89px" : "32px" }}
+            transition={{
+              type: "spring",
+              stiffness: 120,
+              damping: 15,
+              duration: 0.5,
+            }}
             variants={buttonVariants}
-            initial="initial"
             whileHover="hover"
             whileTap="tap"
           >
@@ -70,38 +104,23 @@ const BNavbar = () => {
 
           {/* Contact Icon */}
           <motion.div
-            className="h-8 w-8 relative"
+            className={`w-7 relative ${
+              activePage === "contact" ? "z-10 " : " animate z-0"
+            }`}
             onClick={() => setActivePage("contact")}
+            initial={{ height: "20px" }}
             variants={buttonVariants}
-            initial="initial"
+            animate={{ height: activePage === "contact" ? "86px" : "32px" }}
+            transition={{
+              type: "spring",
+              stiffness: 120,
+              damping: 15,
+              duration: 0.5,
+            }}
             whileHover="hover"
             whileTap="tap"
           >
             <img src={ContactIcon} alt="Contact" className="h-9 w-9" />
-          </motion.div>
-
-          {/* Oval Moving */}
-          <motion.div
-            className={`absolute bottom-[0px] w-[102px] h-[72px] bg-transparent`}
-            initial={false}
-            animate={{
-              x:
-                activePage === "home"
-                  ? "-120%" 
-                  : activePage === "project"
-                  ? "0%" // Move the oval to the center (project icon)
-                  : "120%", // Move the oval to surround the contact icon
-            }}
-            transition={{ type: "spring", stiffness: 120, damping: 15 }}
-          >
-            <motion.img
-              src={oval}
-              alt="Oval"
-              className="w-[72px] h-[72px] absolute"
-              style={{
-                top: "-18px", // Adjust this as needed for correct vertical alignment
-              }}
-            />
           </motion.div>
         </div>
       </nav>
